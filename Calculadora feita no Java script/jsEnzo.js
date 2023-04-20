211864c6a954c0ddbbdd986148e165aab1c6b7de
ZoenSantos <125597511+ZoenSantos@users.noreply.github.com>
2023-04-20 12:38:34 -0300

    Add files via upload

---
+++ Calculadora feita no Java script/jsEnzo.js
@@ -0,0 +1,18 @@
+//Formula para fazer ao cubo//
+function triplique(){
+    var var1 = document.getElementById("X_cubo");
+    document.getElementById("Resultado_ao_cubo").innerText = var1.value*var1.value*var1.value;
+} 
+
+//Fórmula para fazer a área de um triângulo//
+function triangulo(){
+    var var1 = document.getElementById("Base_do_triangulo");
+    var var2 = document.getElementById("Altura_do_triangulo");
+    document.getElementById("Area_do_triangulo").innerText= var1.value*var2.value/2;
+}
+
+//Calcular o valor com desconto de 5% está no html//
+
+
+
+
